895924ce4ed56a8a77922f96154a63ee82909f41
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-08-28 21:53:09 +0000

    Also print out the additive persistence.

--- digital-root/python/digital-root/digital-root.py
+++ digital-root/python/digital-root/digital-root.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
-def digital_root(n):
+def digital_root(n, a=1):
     r = sum(int(digit) for digit in str(n))
     if r < 10:
-        return r
+        return r, a
     else:
-        return digital_root(r)
+        return digital_root(r, a+1)
 
 def readint(prompt):
     while True:
@@ -16,4 +16,6 @@ def readint(prompt):
             print("'{}' is not a valid integer. Try again.".format(s))
 
 num = readint("Enter an integer")
-print("The digital root of {} is {}".format(num, digital_root(num)))
+root, persistence = digital_root(num)
+print("The digital root of {} is {}".format(num, root))
+print("The additive persitence of {} is {}".format(num, persistence))
