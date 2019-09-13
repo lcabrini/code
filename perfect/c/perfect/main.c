316ec87e85edbae8f82fb4e377c86bbf969a1c81
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-09-13 22:53:55 +0000

    Added implementation of perfect in C.

---
+++ perfect/c/perfect/main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("%d %s a perfect number.\n", n, is_perfect(n) ? "is" : "is not");
+
+    return EXIT_SUCCESS;
+}
+
+bool is_perfect(int n)
+{
+    int i, t;
+
+    t = 0;
+    for (i = 1; i < n; ++i) {
+        if (n % i == 0) {
+            t += i;
+        }
+    }
+
+    return t == n;
+}
---
+++ perfect/c/perfect/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool is_perfect(int n);
+
+#endif /* MAIN_H */
